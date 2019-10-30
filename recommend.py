9cb6b83a4a63d696981b0d2ee5b932ef8a1bdbe0
Max Humber <max.humber@gmail.com>
2019-10-30 15:04:28 -0400

    itemvectorizer wip

--- recommend.py
+++ recommend.py
@@ -1,44 +1,45 @@
-import pandas as pd
 import numpy as np
+from scipy.spatial.distance import cdist
+
+import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.neighbors import NearestNeighbors
-from sklearn.metrics.pairwise import euclidean_distances
 
-x = [0, 0, 1, 1, 0, 0, 1]
-y = [0, 0, 1, 1, 1, 0, 1]
-
-def dist(x, y):
-    return np.sqrt(np.dot(x, x) - 2 * np.dot(x, y) + np.dot(y, y))
-
-dist(x, y)
+# X = np.random.binomial(1, p=0.5, size=(1000, 5000))
 
 X = np.array([
     [0, 0, 1, 0, 1, 0, 1],
     [1, 1, 0, 1, 0, 0, 1],
     [0, 0, 0, 0, 1, 0, 1],
     [0, 1, 1, 0, 1, 0, 0],
-    [0, 1, 0, 1, 1, 1, 0]
+    [0, 1, 0, 1, 1, 1, 0],
+    [0, 1, 0, 0, 0, 1, 0],
+    [1, 0, 0, 1, 1, 0, 1],
 ])
 
-%%timeit
-euclidean_distances(X)
+class NearestNeighbors:
+    def __init__(self, n_neighbors=5):
+        self.n_neighbors = n_neighbors
 
-%%timeit
-D = np.zeros((X.shape[0], X.shape[0]))
-for x, xi in enumerate(X):
-    for y, xj in enumerate(X):
-        D[x, y] = dist(xi, xj)
+    def __repr__(self):
+        return f'NearestNeighbors(n_neighbors={self.n_neighbors})'
 
-def dist(x, y):
-    return np.sqrt(np.dot(x, x) - 2 * np.dot(x, y) + np.dot(y, y))
+    def fit(self, X):
+        self.X = X
+        return self
 
+    def kneighbors(self, X, return_distance=False):
+        distances = cdist(X, self.X)
+        neighbors = np.argsort(distances)[:, :self.n_neighbors]
+        if return_distance:
+            return distances, neighbors
+        return neighbors
 
-def euclidean_distances(X):
-    distances = np.zeros((X.shape[0], X.shape[0]))
-    for x, xi in enumerate(X):
-        for y, xj in enumerate(X):
-            distances[x, y] = dist(xi, xj)
-    return distances
+nn = NearestNeighbors(n_neighbors=3)
+nn.fit(X)
+nn.kneighbors(X[0].reshape(1, -1))
+
+np.array([[0, 2, 3]])
+
 
 
 
@@ -51,8 +52,67 @@ df = pd.DataFrame([
     [6, 'k,l'],
 ], columns=['user', 'items'])
 
+X = df['items'].values.tolist()
+X
+X[0]
+
+X = df['items']
+
+max_features = 5
+delimiter = ','
+items = []
+for row in X:
+    if len(items) > max_features:
+        break
+    for item in row.split(delimiter):
+        if item not in items:
+            items.append(item)
+
+Xt = np.zeros((len(X), len(items)), dtype=int)
+for i, row in enumerate(X):
+    for item in row.split(delimiter):
+        try:
+            idx = items.index(item)
+            Xt[i, idx] = 1
+        except ValueError:
+            pass
+Xt
+
+#####
+
+class ItemVectorizer:
+    def __init__(self, delimiter=',', max_features=None):
+        self.delimiter = delimiter
+        if max_features:
+            self.max_features = max_features
+        else:
+            self.max_features = np.inf
+
+    def fit(self, X):
+        self.items = []
+        for row in X:
+            if len(self.items) > self.max_features:
+                break
+            for item in row.split(self.delimiter):
+                if item not in self.items:
+                    self.items.append(item)
+        return self
+
+    def transform(self, X):
+        Xt = np.zeros((len(X), len(self.items)), dtype=int)
+        for i, row in enumerate(X):
+            for item in row.split(self.delimiter):
+                try:
+                    idx = self.items.index(item)
+                    Xt[i, idx] = 1
+                except ValueError:
+                    pass
+        return Xt
+
+
 cv = CountVectorizer(tokenizer=lambda x: x.split(","))
 X = cv.fit_transform(df['items'])
+cv.
 X.todense()
 
 class NNRecommender:
---
+++ test_recommend.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pytest
+
+from recommend import NearestNeighbors
+
+@pytest.fixture
+def X():
+    X = np.array([
+        [0, 0, 1, 0, 1, 0, 1],
+        [1, 1, 0, 1, 0, 0, 1],
+        [0, 0, 0, 0, 1, 0, 1],
+        [0, 1, 1, 0, 1, 0, 0],
+        [0, 1, 0, 1, 1, 1, 0],
+        [0, 1, 0, 0, 0, 1, 0],
+        [1, 0, 0, 1, 1, 0, 1],
+    ])
+    return X
