70bfc5da12f8a6bc989faecee85f532f3c5fe60c
Max Humber <max.humber@gmail.com>
2019-11-05 14:58:59 -0500

    add travis

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="recommend",
-    version="0.2.1",
+    version="0.2.2",
     description="Implicit Recommender",
     long_description=long_description,
     long_description_content_type="text/markdown",
